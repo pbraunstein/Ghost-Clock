ebca429e2ffab37447c4d3a2618dc66d38fec8af
pbraunstein <pbraunstein12@gmail.com>
2015-12-08 22:20:07 -0500

    Restructured ghost.py

---
+++ README.md
@@ -0,0 +1,3 @@
+### Ghost Clock
+Make mouse do a ghost dance at the top of every hour to represent the hour
+passing
--- ghost.py
+++ ghost.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 import pyautogui as p
 import time
+from sys import argv
 
-p.moveTo(100, 100, duration=0.75)
-while True:
-    p.moveTo(1200, 100, duration=0.75)
-    p.moveTo(1200, 800, duration=0.75)
-    p.moveTo(100, 800, duration=0.75)
+def main():
     p.moveTo(100, 100, duration=0.75)
-    time.sleep(1)
+    for x in range(int(argv[1])):
+        p.moveTo(1200, 100, duration=0.75)
+        p.moveTo(1200, 800, duration=0.75)
+        p.moveTo(100, 800, duration=0.75)
+        p.moveTo(100, 100, duration=0.75)
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()

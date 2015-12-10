dd937210bc44bb1cbfe20d7706dd7f53a0f93a05
pbraunstein <pbraunstein12@gmail.com>
2015-12-10 15:35:16 -0500

    Used screen resolution to get caluculate the corners

--- ghost.py
+++ ghost.py
@@ -2,14 +2,34 @@
 import pyautogui as p
 import time
 from sys import argv
+from Quartz import CGDisplayBounds
+from Quartz import CGMainDisplayID
+
+# Constants
+OFF  = 100
 
 def main():
-    p.moveTo(100, 100, duration=0.75)
+    # Get screen bounds
+    infos = CGDisplayBounds(CGMainDisplayID())
+    w = infos.size.width
+    h = infos.size.height
+
+    # Calculate corners
+    UL_X = OFF
+    UL_Y = OFF
+    UR_X = w - OFF
+    UR_Y = OFF
+    LR_X = w - OFF
+    LR_Y = h - OFF
+    LL_X = OFF
+    LL_Y = h - OFF
+
+    p.moveTo(UL_X, UL_Y, duration=0.75)
     for x in range(int(argv[1])):
-        p.moveTo(1200, 100, duration=0.75)
-        p.moveTo(1200, 800, duration=0.75)
-        p.moveTo(100, 800, duration=0.75)
-        p.moveTo(100, 100, duration=0.75)
+        p.moveTo(UR_X, UR_Y, duration=0.75)
+        p.moveTo(LR_X, LR_Y, duration=0.75)
+        p.moveTo(LL_X, LL_Y, duration=0.75)
+        p.moveTo(UL_X, UL_Y, duration=0.75)
         time.sleep(1)
 
 

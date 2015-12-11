726bf2bec1a0fcbda5a11914bad0a3ec836a6e82
pbraunstein <pbraunstein12@gmail.com>
2015-12-11 08:42:29 -0500

    Changed current dir so that it would work

--- master.py
+++ master.py
@@ -3,7 +3,9 @@ import subprocess as sp
 import os
 from datetime import datetime
 
-GHOST_SCRIPT = os.path.join(os.getcwd(), 'ghost.py')
+
+SCRIPT_DIR = "/Users/Phippe/Developer/ghostCron" 
+GHOST_SCRIPT = os.path.join(SCRIPT_DIR, 'ghost.py')
 
 def main():
     reps = str(datetime.now().time().hour % 12)  # Mod to make it 12 hour

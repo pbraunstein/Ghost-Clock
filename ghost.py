b3e2359d4b0cca430f9ea1f39c29f76edf730d3f
pbraunstein <pbraunstein12@gmail.com>
2015-12-13 09:31:57 -0500

    Added slow sweep to start back in. I think it helps get the user's attention so that they don't miss the chime

--- ghost.py
+++ ghost.py
@@ -30,7 +30,7 @@ def main():
 
 # Traces numChimes number of squares with the mouse
 def hourChime(numChimes):
-    p.moveTo(UL_X, UL_Y)
+    p.moveTo(UL_X, UL_Y, duration=SWEEP_TIME)
     for x in range(numChimes):
         p.moveTo(UR_X, UR_Y, duration=SWEEP_TIME)
         p.moveTo(LR_X, LR_Y, duration=SWEEP_TIME)
@@ -41,7 +41,7 @@ def hourChime(numChimes):
 
 # Traces a triangle on the screen with the mouse
 def fifteenChime():
-    p.moveTo(UM_X, UM_Y);
+    p.moveTo(UM_X, UM_Y, duration=SWEEP_TIME);
     p.moveTo(LR_X, LR_Y, duration=SWEEP_TIME)
     p.moveTo(LL_X, LL_Y, duration=SWEEP_TIME)
     p.moveTo(UM_X, UM_Y, duration=SWEEP_TIME)
@@ -49,12 +49,12 @@ def fifteenChime():
 
 # Traces a horizontal line on the screen with the mouse
 def thirtyChime():
-    p.moveTo(ML_X, ML_Y);
+    p.moveTo(ML_X, ML_Y, duration=SWEEP_TIME);
     p.moveTo(MR_X, MR_Y, duration=SWEEP_TIME)
 
 
 def fortyfiveChime():
-    p.moveTo(LM_X, LM_Y);
+    p.moveTo(LM_X, LM_Y, duration=SWEEP_TIME);
     p.moveTo(UL_X, UL_Y, duration=SWEEP_TIME)
     p.moveTo(UR_X, UR_Y, duration=SWEEP_TIME)
     p.moveTo(LM_X, LM_Y, duration=SWEEP_TIME)
